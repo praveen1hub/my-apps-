b5841bf50a7cbe24b281949446c68c6fa06c50a7
praveenlattala <146346859+praveenlattala@users.noreply.github.com>
2026-06-15 15:45:27 +0530

    Remove all files

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM node:20-alpine
-WORKDIR /app
-COPY . .
-EXPOSE 3000
-CMD ["node", "index.js"]
--- index.js
+++
@@ -1,5 +0,0 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  res.end('Hello from my CI/CD pipeline!');
-});
-server.listen(3000, () => console.log('Running on port 3000'));
